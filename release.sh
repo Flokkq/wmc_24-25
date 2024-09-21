f935f82fec9cf4119efb19fb050ff55896195fc6
Flokkq <webcla21@htl-kaindorf.at>
2024-09-21 20:15:07 +0200

    chore(git-cliff): add release script

---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if ! command -v typos &>/dev/null; then
+  echo "typos is not installed. Run 'cargo install typos-cli' to install it, otherwise the typos won't be fixed"
+fi
+
+if [ -z "$1" ]; then
+	echo "Please provide a tag."
+	echo "Usage: ./release.sh v[X.Y.Z]"
+	exit
+fi
+
+echo "Preparing $1..."
+msg="# managed by release.sh"
+
+# update the changelog
+git-cliff --config cliff.toml --tag "$1" > CHANGELOG.md
+git add -A && git commit -m "chore(release): prepare for $1"
+git show
+
+# generate a changelog for the tag message
+export GIT_CLIFF_TEMPLATE="\
+	{% for group, commits in commits | group_by(attribute=\"group\") %}
+	{{ group | upper_first }}\
+	{% for commit in commits %}
+		- {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
+	{% endfor %}
+	{% endfor %}"
+
+changelog=$(git-cliff --config detailed.toml --unreleased --strip all)
+
+# create a signed tag
+# https://keyserver.ubuntu.com/pks/lookup?search=0x4A92FA17B6619297&op=vindex
+git tag -s -a "$1" -m "Release $1" -m "$changelog"
+git tag -v "$1"
+echo "Done!"
+echo "Now push the commit (git push) and the tag (git push --tags)."
